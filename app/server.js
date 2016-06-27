ad677201143a56b0cb1d0eaa0b8503285429ed35
berta <berta.besteiro@gmail.com>
2016-06-27 13:07:40 +0200

    mitad para ver cambios

--- app/server.js
+++ app/server.js
@@ -1,24 +1,37 @@
 var express = require('express') //llamamos a Express
 var app = express()
 
+// para establecer las distintas rutas, necesitamos instanciar el express router
+var router = express.Router()
+
+
 var port = process.env.PORT || 8080 // establecemos nuestro puerto
 
-app.get('/', function(req, res) {
+/*router.get('/', function(req, res) {
   res.json({ mensaje: '¡Hola Mundo!' })
-})
+})*/
 
-app.get('/cervezas', function(req, res) {
+/*router.get('/cervezas', function(req, res) {
   res.json({ mensaje: '¡A beber cerveza!' })
+})*/
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+router.post('/', function(req, res) {
+  res.json({ mensaje: '¡Hola ' + req.body.nombre + ' con parametro!' })
 })
 
-app.post('/', function(req, res) {
+/*router.post('/', function(req, res) {
   res.json({ mensaje: 'Método post' })
 })
 
-app.delete('/', function(req, res) {
+router.delete('/', function(req, res) {
   res.json({ mensaje: 'Método delete' })
-})
+})*/
 
 // iniciamos nuestro servidor
+app.use('/api', router)
 app.listen(port)
 console.log('API escuchando en el puerto ' + port)
