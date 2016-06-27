7aa11e9698130e086a7d8e2ae321ee6da22ebbbf
berta <berta.besteiro@gmail.com>
2016-06-27 13:35:27 +0200

    API preparada para seguir con mongodb

--- app/server.js
+++ app/server.js
@@ -1,37 +1,17 @@
 var express = require('express') //llamamos a Express
 var app = express()
-
-// para establecer las distintas rutas, necesitamos instanciar el express router
-var router = express.Router()
-
+var bodyParser = require('body-parser')
 
 var port = process.env.PORT || 8080 // establecemos nuestro puerto
 
-/*router.get('/', function(req, res) {
-  res.json({ mensaje: '¡Hola Mundo!' })
-})*/
-
-/*router.get('/cervezas', function(req, res) {
-  res.json({ mensaje: '¡A beber cerveza!' })
-})*/
-
-var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-router.post('/', function(req, res) {
-  res.json({ mensaje: '¡Hola ' + req.body.nombre + ' con parametro!' })
-})
-
-/*router.post('/', function(req, res) {
-  res.json({ mensaje: 'Método post' })
-})
-
-router.delete('/', function(req, res) {
-  res.json({ mensaje: 'Método delete' })
-})*/
-
-// iniciamos nuestro servidor
+// nuestra ruta irá en http://localhost:8080/api
+// es bueno que haya un prefijo, sobre todo por el tema de versiones de la API
+var router = require('./routes')
 app.use('/api', router)
+
+//arrancamos el servidor
 app.listen(port)
 console.log('API escuchando en el puerto ' + port)
